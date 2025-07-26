b70b443e44cb2ed4f4ff748dbe5b33ac0cd80c4e
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 23:07:49 +0300

    Update src/app/not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl">404 - Page Not Found</h1>
+      <h2 className="text-xl font-bold">Page Not Found</h2>
     </div>
   )
 }
