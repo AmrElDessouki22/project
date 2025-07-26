3f068f2cf75303c4af06d5cb45ee78426ed198e6
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-26 23:08:00 +0300

    Update src/hooks/useApi.ts

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -2,27 +2,29 @@
 
 import { useState, useEffect } from 'react'
 
-function useApi(url: string) {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+export function useApi<T>(url: string) {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<Error | null>(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
         const result = await response.json()
         setData(result)
       } catch (error) {
-        setError(error)
+        setError(error as Error)
       } finally {
         setLoading(false)
       }
     }
+
     fetchData()
   }, [url])
 
   return { data, loading, error }
 }
-
-export default useApi
