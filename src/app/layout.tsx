b4133e8668810cee846485571a4a9946a7f6cc7f
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:57:06 +0300

    Update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,25 @@
-import { ReactNode } from 'react';
 import '@/app/globals.css';
-import { Header } from '@/components/ui/Header';
-import { Footer } from '@/components/ui/Footer';
-import { NotesProvider } from '@/contexts/NotesContext';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
+import Header from '@/components/ui/Header';
+import Footer from '@/components/ui/Footer';
+
+export const metadata: Metadata = {
+  title: 'Notes App',
+  description: 'A comprehensive notes application.',
+};
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-export const metadata = {
-  title: 'Note App',
-  description: 'A comprehensive note-taking application.',
-};
-
 const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
-      <body>
-        <NotesProvider>
-          <Header />
-          <main>{children}</main>
-          <Footer />
-        </NotesProvider>
+      <body className="bg-gray-100 text-gray-900">
+        <Header />
+        <main className="container mx-auto p-4">{children}</main>
+        <Footer />
       </body>
     </html>
   );
