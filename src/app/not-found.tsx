944821fdd63fbe5f98ab97625ff63f15074a54ed
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:57:08 +0300

    Update src/app/not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
-const NotFound = () => {
+const NotFoundPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex justify-center items-center h-screen">
       <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
     </div>
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
