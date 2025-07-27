b1f3e70f63208bf203f7cbcb18027488bd04b367
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:21:22 +0300

    Update src/app/loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,11 @@
-export default function Loading() {
+const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <p>Loading...</p>
+      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Loading;
