1213efe76c4bb94e636692542bf21fcc2a2190ef
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-28 21:39:25 +0300

    Update src/components/ui/Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-600 text-white p-4 mt-4 text-center">
-      &copy; 2023 Note App
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <p>&copy; 2023 Next.js Notes App</p>
     </footer>
   );
 }
