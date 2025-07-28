0195ae2671e2440bb73cdd21a3654ab465998947
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-28 21:21:17 +0300

    Update src/app/error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,36 +1,12 @@
 'use client';
 
-import { Component, ErrorInfo, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
-interface ErrorBoundaryProps {
-  children: ReactNode;
+export default function ErrorBoundary({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      {children}
+    </div>
+  );
 }
-
-interface ErrorBoundaryState {
-  hasError: boolean;
-}
-
-class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children;
-  }
-}
-
-export default ErrorBoundary;
