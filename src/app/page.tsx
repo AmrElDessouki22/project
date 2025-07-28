7928d075fa2999d9f39ebd00fb770ab29cc3bb27
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-28 21:39:19 +0300

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { NotesList } from '@/components/features/NotesList';
-import AddNote from '@/components/features/AddNote';
+import NotesList from '../components/features/NotesList';
+import AddNote from '../components/features/AddNote';
 
 export default function HomePage() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">My Notes</h1>
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Notes</h1>
       <AddNote />
       <NotesList />
     </div>
