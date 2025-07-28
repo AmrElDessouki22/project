3bf3716d184718511b82c3bedbd66f85fce886c9
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-28 21:39:09 +0300

    Update src/app/error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,12 +1,3 @@
-'use client';
-
-import { ReactNode } from 'react';
-
-export default function ErrorBoundary({ children }: { children: ReactNode }) {
-  return (
-    <div>
-      <h1>Something went wrong!</h1>
-      {children}
-    </div>
-  );
+export default function Error() {
+  return <div>An error occurred. Please try again later.</div>;
 }
