2c016ecbc50b328a0a878255fb76a03ca1acc556
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-28 21:21:32 +0300

    Update src/components/ui/Header.tsx

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,9 @@
-const Header = () => {
+'use client';
+
+export default function Header() {
   return (
-    <header className="bg-blue-600 text-white py-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">Notes App</h1>
-      </div>
+    <header className="bg-blue-600 text-white p-4">
+      <h1 className="text-xl">Note App</h1>
     </header>
   );
-};
-
-export default Header;
+}
