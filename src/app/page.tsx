61e93c6d5d09feea6dc70f44ed0bd02adf5b6960
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:11:29 +0300

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import NotesList from '@/components/features/NotesList';
-import AddNoteForm from '@/components/features/AddNoteForm';
+import NoteList from '@/components/features/NoteList';
+import NoteForm from '@/components/features/NoteForm';
 
-const HomePage: React.FC = () => {
+export default function HomePage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">My Notes</h1>
-      <AddNoteForm />
-      <NotesList />
+      <h1 className="text-2xl font-bold mb-4">Notes</h1>
+      <NoteForm />
+      <NoteList />
     </div>
   );
-};
-
-export default HomePage;
+}
