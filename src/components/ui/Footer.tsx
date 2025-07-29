0fbfd58ced3f0eb9563f35e59facd7448f98f715
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 12:48:35 +0300

    Update src/components/ui/Footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const Footer: React.FC = () => {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      <p>&copy; 2023 Note App. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
+export const Footer: React.FC = () => (
+  <footer className="bg-gray-800 text-white p-4 mt-8">
+    <p className="text-center">&copy; 2023 Notes App. All rights reserved.</p>
+  </footer>
+);
