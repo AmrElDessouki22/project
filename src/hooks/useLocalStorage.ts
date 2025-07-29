f8ef8002042a655f14c84840c0acedde4137f2fa
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 11:59:20 +0300

    Update src/hooks/useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,25 +1,34 @@
+'use client';
+
 import { useState } from 'react';
 
-export default function useLocalStorage(key, initialValue) {
-  const [storedValue, setStoredValue] = useState(() => {
+function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
     try {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return initialValue;
     }
   });
 
-  const setValue = (value) => {
+  const setValue = (value: T | ((val: T) => T)) => {
     try {
       const valueToStore = value instanceof Function ? value(storedValue) : value;
       setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
-  return [storedValue, setValue];
+  return [storedValue, setValue] as const;
 }
+
+export default useLocalStorage;
