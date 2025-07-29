e54ed396e80c6ca26a2ce47c38cc01a65464e28c
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:11:26 +0300

    Update src/app/loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-
-const Loading: React.FC = () => {
-  return <div className="text-center">Loading...</div>;
-};
-
-export default Loading;
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader">Loading...</div>
+    </div>
+  );
+}
