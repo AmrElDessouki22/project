79185431ef3961e226545791e9c54ee9f6e365bf
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 14:33:18 +0300

    Update src/app/loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Loading: React.FC = () => (
-  <div className="flex items-center justify-center h-screen">
-    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-  </div>
-);
+const Loading: React.FC = () => {
+  return <div className="text-center">Loading...</div>;
+};
 
 export default Loading;
