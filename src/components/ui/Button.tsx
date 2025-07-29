641aab1f12a717c3bc8a562c4ec56173baecd551
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 14:33:22 +0300

    Update src/components/ui/Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,19 @@
 'use client';
 import React from 'react';
 
-interface ButtonProps {
+type ButtonProps = {
   onClick: () => void;
   children: React.ReactNode;
-  isDelete?: boolean;
-}
+};
 
-export const Button: React.FC<ButtonProps> = ({ onClick, children, isDelete = false }) => (
-  <button
-    onClick={onClick}
-    className={`font-bold py-2 px-4 rounded ${isDelete ? 'bg-red-600 hover:bg-red-700 text-white' : 'bg-blue-600 hover:bg-blue-700 text-white'}`}
-  >
-    {children}
-  </button>
-);
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button 
+      onClick={onClick} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
