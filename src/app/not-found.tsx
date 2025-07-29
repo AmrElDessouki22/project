2960dd17bff8bf722a48cf085c7f9ffde09297e6
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 11:59:02 +0300

    Update src/app/not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,11 @@
-export default function NotFound() {
-  return <div>404 - Page Not Found</div>;
-}
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl">404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
