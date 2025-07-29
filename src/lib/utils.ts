15638346e8fe2bb7cb22aaea7e28a7894bf15ead
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 12:48:45 +0300

    Update src/lib/utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
-export const generateId = (): string => {
-  return Math.random().toString(36).substr(2, 9);
+export const formatDate = (date: Date): string => {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit'
+  }).format(date);
 };
